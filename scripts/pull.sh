51ac892b071b8b433460d3785733498c626453be
liblaf <30631553+liblaf@users.noreply.github.com>
2025-03-01 21:20:54 +0800

    vault backup: 2025-03-01 21:20:54
    Affected files:
    scripts/pull.sh

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-set -o errexit -o nounset -o pipefail
+set -o errexit
+set -o nounset
+set -o pipefail
 
-command rsync --info="progress2" --archive --force --stats --human-readable --progress --itemize-changes "$HOME/SeaDrive/My Libraries/webdav/obsidian/" .
+command rsync --info="progress2" --archive --force --stats --human-readable --progress --itemize-changes "$HOME/net/SeaDrive/My Libraries/webdav/obsidian/" .
